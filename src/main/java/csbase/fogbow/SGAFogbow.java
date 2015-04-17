b244357bcd6c68664aa4253f82c85fe564535d63
Abmar Barros <abmargb@gmail.com>
2015-04-17 14:08:22 -0300

    instantiating both executor and monitor inside the init() method

--- src/main/java/csbase/fogbow/SGAFogbow.java
+++ src/main/java/csbase/fogbow/SGAFogbow.java
@@ -7,6 +7,10 @@ public class SGAFogbow extends SGALocal {
 
 	public SGAFogbow(IServiceManager serviceManager) {
 		super(serviceManager);
+	}
+
+	@Override
+	protected void init() {
 		setExecutor(new FogbowExecutor(pluginProperties));
 		setMonitor(new FogbowMonitor());
 	}
